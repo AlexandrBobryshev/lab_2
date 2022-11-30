015803057acdd583cfcc352a3ec5b859581fa6b0
AlexandrBobryshev <91112389+AlexandrBobryshev@users.noreply.github.com>
2022-11-30 21:28:22 +0100

    Add files via upload

---
+++ program.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+f = open('DS3.txt', 'r')
+x = []
+y = []
+
+while True:
+    line = f.readline()
+    if not line:
+        break
+    x.append(int(line.split()[0]))
+    y.append(int(line.split()[1]))
+
+plt.rcParams['toolbar'] = 'None'
+plt.figure(figsize=(960/plt.rcParams['figure.dpi'], 540/plt.rcParams['figure.dpi']))
+plt.scatter(x, y)
+plt.axis('off')
+plt.savefig('result.jpg')
+f.close()
